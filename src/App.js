1bc7c27efbfb874f4e5f9f8a000c47f3fa03bf0a
rahul6382 <rahulrahe999@gmail.com>
2021-02-22 14:52:52 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Barchart from './components/barchart';
+import Linechart from './components/linechart';
+import Piechart from './components/piechart';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Barchart/>
+        <Linechart/>
+        <Piechart/>
     </div>
   );
 }
